20e6704d5765cfd8d6bd5cb881f128d007d75d54
Anmol H <160043107+Anmol0240@users.noreply.github.com>
2026-03-13 13:03:24 +0530

    hardcode api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://zoop-app.onrender.com';
+const API_URL = 'https://zoop-app.onrender.com';
 
 export const api = axios.create({ baseURL: `${API_URL}/api` });
 export const SOCKET_URL = API_URL;
